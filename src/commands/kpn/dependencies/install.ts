e288eae078ce281502acee84cd6653bd80172c57
Dieffrei Quadros <dieffrei@gmail.com>
2019-08-05 14:26:25 +0200

    initial commit

---
+++ src/commands/kpn/dependencies/install.ts
@@ -0,0 +1,48 @@
+import {flags, SfdxCommand} from '@salesforce/command';
+import {Messages} from '@salesforce/core';
+import {AnyJson} from '@salesforce/ts-types';
+import {Connection} from 'jsforce';
+// tslint:disable-next-line:no-var-requires
+const spawn = require('child-process-promise').spawn;
+
+Messages.importMessagesDirectory(__dirname);
+
+const messages = Messages.loadMessages('sfdx-soap-login', 'login');
+
+export default class Install extends SfdxCommand {
+
+  public static description = messages.getMessage('commandDescription');
+
+  public static examples = [
+    `$ sfdx kpn:dependencies:login --username myOrg@example.com --password pass123 --loginurl https://test.salesforce.com
+  `];
+
+  protected static flagsConfig = {
+    username: flags.string({char: 'u', description: 'username'}),
+    password: flags.string({char: 'p', description: 'password'}),
+    loginurl: flags.string({char: 'l', description: 'm'})
+  };
+
+  protected static requiresProject = false;
+
+  public async run(): Promise<AnyJson> {
+
+    let loginUrl = (this.flags.loginurl == null)
+      ? 'https://login.salesforce.com'
+      : this.flags.loginurl;
+
+    const conn = new Connection({
+      loginUrl: loginUrl
+    });
+
+    await conn.loginBySoap(this.flags.username, this.flags.password);
+    await spawn('sfdx', ['force:config:set', `instanceUrl=${conn.instanceUrl}`], { stdio: 'inherit' });
+    await spawn('sfdx', ['texei:package:dependencies:install', `--targetusername=${conn.accessToken}`], { stdio: 'inherit' });
+
+    return {
+      instanceUrl: conn.instanceUrl,
+      accessToken: conn.accessToken
+    };
+
+  }
+}
